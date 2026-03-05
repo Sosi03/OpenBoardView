Sosi03/OpenBoardView

commit e238d63a082aabcebd31cc0d9d5be864344d021e

High-DPI support on Windows
Disabled for Linux because it is broken and very inconsistent across Xorg/XWayland/Wayland using SDL2. Maybe SDL3 will be better but it is too recent.

macOS situation unknown.

src/openboardview/main_opengl.cpp
@@ -216,6 +216,10 @@ int main(int argc, char **argv) {
 	// Log all messages
 	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
 
+#if SDL_VERSION_ATLEAST(2, 24, 0)
+	SDL_SetHint(SDL_HINT_WINDOWS_DPI_AWARENESS, "system");
+#endif
+
 	/*
 	 * Parse the parameters first up, store the results in the global struct.
 	 *
@@ -274,11 +278,19 @@ int main(int argc, char **argv) {
 		g.renderer = Renderers::get(app.obvconfig.ParseInt("renderer", static_cast<int>(Renderers::Preferred)));
 	}
 
+	float main_scale = ImGuiRendererSDL::getDisplayScale();
+	setDisplayScale(main_scale);
+
+	int window_width = g.width * main_scale;
+	int window_height = g.height * main_scale;
+
 	// Setup window
-	SDL_DisplayMode current;
-	SDL_GetCurrentDisplayMode(0, &current);
+	uint32_t window_flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+#ifdef _WIN32
+	window_flags |= SDL_WINDOW_ALLOW_HIGHDPI;
+#endif
 	window = SDL_CreateWindow(
-	    OBV_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, g.width, g.height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	    OBV_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, window_height, window_flags);
 	if (window == NULL) {
 		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create the sdlWindow: %s\n", SDL_GetError());
 		cleanupAndExit(1);
@@ -302,6 +314,11 @@ int main(int argc, char **argv) {
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable ImGui's keyboard navigation
 	io.IniFilename = NULL;
 	//	io.Fonts->AddFontDefault();
+
+	// Apply system display scale factor to ImGui
+	ImGuiStyle& style = ImGui::GetStyle();
+	style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+	style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
 
 	// Main loop
 	bool done             = false;

src/openboardview/BoardView.cpp
@@ -73,7 +73,7 @@ int BoardView::ConfigParse(void) {
 	style.AntiAliasedLines = !config.slowCPU;
 	style.AntiAliasedFill  = !config.slowCPU;
 
-	m_info_surface.x = config.infoPanelWidth;
+	m_info_surface.x = DPIF(config.infoPanelWidth); // Convert to DPI-dependent value
 	backgroundImage.enabled = config.showBackgroundImage;
 
 	m_colors.readFromConfig(obvconfig);
@@ -244,7 +244,7 @@ void BoardView::ShowInfoPane(void) {
 		}
 	} else {
 		if (m_dragging_token == 2) {
-			config.infoPanelWidth = m_info_surface.x;
+			config.infoPanelWidth = IDPIF(m_info_surface.x); // Convert back to DPI-independant value
 			obvconfig.WriteInt("infoPanelWidth", m_info_surface.x);
 		}
 		m_dragging_token = 0;

src/openboardview/GUI/DPI.cpp
@@ -4,18 +4,30 @@
 
 static int dpi = 0;
 
+// Scale factor from the platform
+static float display_scale = 1.0f;
+
 float DPIF(float x) {
-	return (x * dpi) / 100.f;
+	return (x * dpi * display_scale) / 100.f;
 }
 
 int DPI(int x) {
-	return (x * dpi) / 100;
+	return (x * dpi * display_scale) / 100;
+}
+
+// Inverse DPIF
+float IDPIF(float x) {
+	return (x * 100.0f) / (dpi * display_scale);
 }
 
 void setDPI(int new_dpi) {
 	dpi = new_dpi;
 	ImGuiStyle &style = ImGui::GetStyle();
 	style.ScrollbarSize *= dpi / 100.0f;
+}
+
+void setDisplayScale(float v) {
+	display_scale = v;
 }
 
 int getDPI() {

src/openboardview/GUI/DPI.h
@@ -3,7 +3,9 @@
 
 float DPIF(float x);
 int DPI(int x);
+float IDPIF(float x);
 void setDPI(int dpi);
+void setDisplayScale(float display_scale);
 int getDPI();
 
 #endif//_DPI_H_

src/openboardview/Renderers/ImGuiRendererSDL.cpp
@@ -9,6 +9,15 @@
 
 #include "utils.h"
 
+float ImGuiRendererSDL::getDisplayScale() {
+	// Scaling from platform display DPI is only supported on Windows for now as it is broken and inconsistent on X11/XWayland/Wayland
+#ifdef _WIN32
+	return ImGui_ImplSDL2_GetContentScaleForDisplay(0);
+#else
+	return 1.0f;
+#endif
+}
+
 ImGuiRendererSDL::ImGuiRendererSDL(SDL_Window *window) : window(window) {
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);

src/openboardview/Renderers/ImGuiRendererSDL.h
@@ -13,6 +13,8 @@
 
 class ImGuiRendererSDL {
 public:
+	static float getDisplayScale();
+
 	explicit ImGuiRendererSDL(SDL_Window *window);
 	virtual ~ImGuiRendererSDL();
 

commit 4cbb55d81d6d67b1a41d084b4ab9a8625c5d87d2

Fix infinite loop caused by negative board surface size
After the change in e238d63, having `windowX=1200`, `dpi=200`, and
`infoPanelWidth = 652` in `obv.conf` on Linux would cause
`app.m_board_surface.x` to be negative, which caused `sx` to be
negative, which caused `m_scale` to be negative, which caused `vdelta`
to be negative, which caused `y` to never reach `yend`, causing an
infinite loop. Fix this by detecting the negative condition early and
setting `app.m_board_surface.x` and `app.m_info_surface.x` to reasonable
values.

Fixes #348.

Fixes: e238d63 ("High-DPI support on Windows")

src/openboardview/main_opengl.cpp
@@ -352,6 +352,10 @@ int main(int argc, char **argv) {
 	app.m_board_surface.x = g.width;
 	app.m_board_surface.y = g.height;
 	if (app.config.showInfoPanel) app.m_board_surface.x -= app.m_info_surface.x;
+	if (app.m_board_surface.x <= 0.0f) {
+		app.m_board_surface.x = g.width * 0.66f;
+		app.m_info_surface.x = g.width - app.m_board_surface.x;
+	}
 
 	if (g.font_size > 0.0) app.config.fontSize = g.font_size;
 