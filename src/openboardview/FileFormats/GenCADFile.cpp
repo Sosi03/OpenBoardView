Sosi03/OpenBoardView

commit edddcff89f504fb4919ca042c090d7b3b68ce5a5

gencad: support filtering out duplicated components

src/openboardview/FileFormats/GenCADFile.cpp
@@ -211,6 +211,13 @@ bool GenCADFile::parse_components() {
 			if (shape_ref_ast) {
 				char *shape_name_str = get_nonquoted_or_quoted_string_child(shape_ref_ast, "shape_name");
 				if (shape_name_str && *shape_name_str) {
+					PositionedNamedShape duplicate_check_id{brd_part.p1.x, brd_part.p1.y, shape_name_str};
+					if (!m_parsed_shapes.insert(duplicate_check_id).second)
+					{
+						i++;
+						continue; //component with duplicate shape&position, ignore it.
+					}
+
 					if (!brd_part.mfgcode.empty()) {
 						brd_part.mfgcode += " SHAPE ";
 					}

src/openboardview/FileFormats/GenCADFile.h
@@ -8,6 +8,7 @@
 #include "mpc/mpc.h"
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -80,6 +81,10 @@ class GenCADFile : public BRDFileBase {
 
 	typedef std::tuple<std::string, std::string> ComponentPin;
 	std::map<ComponentPin, std::string> m_signals_cache;
+
+	// stores all loaded shapes to perform duplicate detection
+	typedef std::tuple<int, int, std::string> PositionedNamedShape;
+	std::set<PositionedNamedShape> m_parsed_shapes;
 	int nc_counter = 0;
 
 };

commit d50887cace18e2510324463db49c327ad7850b45

gencad: fixup "deduplicating" elements placed on opposite sides
Problem was introduces by too aggressive deduplication introduced in
edddcff89f504fb4919ca042c090d7b3b68ce5a5

Fixed by differentiating sides while seeking for duplicates

src/openboardview/FileFormats/GenCADFile.cpp
@@ -214,7 +214,7 @@ bool GenCADFile::parse_components() {
 			if (shape_ref_ast) {
 				char *shape_name_str = get_nonquoted_or_quoted_string_child(shape_ref_ast, "shape_name");
 				if (shape_name_str && *shape_name_str) {
-					PositionedNamedShape duplicate_check_id{brd_part.p1.x, brd_part.p1.y, shape_name_str};
+					PositionedNamedShape duplicate_check_id{brd_part.mounting_side, brd_part.p1.x, brd_part.p1.y, shape_name_str};
 					if (!m_parsed_shapes.insert(duplicate_check_id).second)
 					{
 						i++;

src/openboardview/FileFormats/GenCADFile.h
@@ -89,7 +89,7 @@ class GenCADFile : public BRDFileBase {
 	std::map<std::string, PadStackInfo> m_pad_stack_info_cache;
 
 	// stores all loaded shapes to perform duplicate detection
-	typedef std::tuple<int, int, std::string> PositionedNamedShape;
+	typedef std::tuple<BRDPartMountingSide, int, int, std::string> PositionedNamedShape;
 	std::set<PositionedNamedShape> m_parsed_shapes;
 	int nc_counter = 0;
 